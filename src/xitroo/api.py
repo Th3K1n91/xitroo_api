32bcaa1c19d84a367b1652a357aec8b3c35c7c76
Th3K1n91 <63650014+Th3K1n91@users.noreply.github.com>
2022-05-26 22:09:35 +0200

    Add files via upload

--- src/xitroo/api.py
+++ src/xitroo/api.py
@@ -5,20 +5,15 @@ import requests
 class xitroo():
     def __init__(self, xitroo_email, refresh_counter=5, refresh_wait=5):
         self.xemail = xitroo_email
-        ts = int(format(time.time(), ".0f"))
         for i in range(refresh_counter):
-            idsite = requests.get(f"https://api.xitroo.com/v1/mails?locale=de&mailAddress={self.xemail}&mailsPerPage=25&minTimestamp=0.0&maxTimestamp={str(ts + 500)}").json()
+            idsite = requests.get(f"https://api.xitroo.com/v1/mails?locale=de&mailAddress={self.xemail}&mailsPerPage=25&minTimestamp=0.0&maxTimestamp={str(int(format(time.time(), '.0f')) + 500)}").json()
             try:
-                mails = idsite["mails"][0]
-                id = mails['_id']
-                self.mega = requests.get("https://api.xitroo.com/v1/mail?locale=de&id=" + id).json()
+                self.mega = requests.get("https://api.xitroo.com/v1/mail?locale=de&id=" + idsite["mails"][0]['_id']).json()
                 break
             except:
                 print(idsite['type'])
-                if i == refresh_counter:
-                    break
+                if i == refresh_counter: break
                 time.sleep(refresh_wait)
-
 
     def get_bodyHtmlStrict(self):
         return base64.b64decode(self.mega["bodyHtmlStrict"])
