1ea592a4d7f3845ac01136f491e1db9af7bf8b15
Th3K1n91 <63650014+Th3K1n91@users.noreply.github.com>
2022-05-28 19:57:56 +0200

    Add files via upload

--- src/xitroo/api.py
+++ src/xitroo/api.py
@@ -3,7 +3,7 @@ import time
 import requests
 
 class xitroo():
-    def __init__(self, xitroo_email, refresh_counter=5, refresh_wait=5):
+    def __init__(self, xitroo_email: str, refresh_counter=5, refresh_wait=5):
         self.xemail = xitroo_email
         for i in range(refresh_counter):
             idsite = requests.get(f"https://api.xitroo.com/v1/mails?locale=de&mailAddress={self.xemail}&mailsPerPage=25&minTimestamp=0.0&maxTimestamp={str(int(format(time.time(), '.0f')) + 500)}").json()
@@ -15,11 +15,10 @@ class xitroo():
                 if i == refresh_counter: break
                 time.sleep(refresh_wait)
 
-    def get_bodyHtmlStrict(self):
-        return base64.b64decode(self.mega["bodyHtmlStrict"])
+    def get_bodyHtmlStrict(self): return base64.b64decode(self.mega["bodyHtmlStrict"])
 
-    def get_bodyHtml(self):
-        return base64.b64decode(self.mega["bodyHtml"])
+    def get_bodyHtml(self): return base64.b64decode(self.mega["bodyHtml"])
 
-    def get_bodyText(self):
-        return base64.b64decode(self.mega["bodyText"])
+    def get_bodyText(self): return base64.b64decode(self.mega["bodyText"])
+
+    def get_subject(self): return base64.b64decode(self.mega["subject"])
